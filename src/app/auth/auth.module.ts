68e7e85d7d7236c5093c5032a57fc16db535481f
Jossy2005 <jab.mena@yavirac.edu.ec>
2024-12-10 21:42:24 -0500

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  providers:[
+    AuthService
+  ],
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ButtonModule
+    
+  ]
+})
+export class AuthModule { }
